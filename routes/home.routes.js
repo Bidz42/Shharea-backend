b1e59d3f62926daa54fc2c3850a176b2a14d8497
Bidz42 <abid97r@gmail.com>
2022-09-20 18:15:11 +0100

    upload and comment

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const { isAuthenticated } = require("../middleware/jwt.middleware");
 const User = require("../models/User.model");
 const Upload = require("../models/Upload.model");
+const Comment = require("../models/Comment.model");
 const uploadCloud = require("../config/cloudinary.config");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 
 router.post("/upload", uploadCloud.single("imageUrl"), (req, res, next) => {
@@ -32,9 +32,64 @@ router.get("/images", (req, res) => {
     .catch((err) => console.log(err))
 })
 
+router.get("/image/:id", (req, res) => {
+    const {id} = req.params
+    Upload.findById(id)
+    .populate("owner")
+    // .populate(
+    //     path: "comments likes",
+    //     populate: {
+    //         path: "owner", 
+    //         model: "User",
+    //     },
+        // path: "likes",
+        // populate: {
+        //     path: "owner", //populate the review owner within the review model
+        //     model: "User",
+        // },
+    // })
+    .then((response) => res.status(200).json(response))
+    .catch((err) => console.log(err))
+})
 
+// router.post("/image/comment", (req, res) => {
+//     const {owner, imageId, comment } = req.body;
 
+//     Comment.create({comment, owner})
+//     .then(res => { return Upload.updateOne ( {_id : imageId}, {$push: {comments : [res._id]}})
+//     return Upload.findById(imageId)
+//     .populate('comments owner')
+//         .populate({
+//             path: "comment",
+//             populate: {
+//                 path: "owner", 
+//                 model: "User",
+//             },
+//     })
+//     .then((response) => {
+//         console.log(response.comments)
+//         res.status(200).json(response)})
+//     .catch((err) => console.log(err))
+// });
 
+router.post(`/image/comment`, (req, res) =>{
+    const {comment, owner, imageId} = req.body;
+    Comment.create({comment, owner})
+        .then(response => { return Upload.updateOne( {_id : imageId}, {$push: {comments : [response._id]}})})
+    return Upload.findById(imageId)
+        .populate('comments owner')
+        .populate({
+            path: "comments",
+            populate: {
+                path: "owner", //populate the review owner within the review model
+                model: "User",
+  },
+})
+        .then(response => {
+            console.log(response.comments)
+            res.status(200).json(response)})
+        .catch((err) => console.log(err));
+});
 
 
 module.exports = router;
