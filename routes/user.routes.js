695086aae2ccc1d36b0488981f8ba1577f91a4dd
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-20 23:02:02 +0200

    profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,23 @@
-//get profgile
+const express = require("express");
+const Upload = require("../models/Upload.model");
+const router = express.Router();
+const User = require("../models/User.model");
+// const Upload = require("../models/Upload.model");
+// const Comment = require("../models/Comment.model");
+// const uploadCloud = require("../config/cloudinary.config");
+// const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-//edit profile
+router.get("/profile/:id", (req,res) => {
+    const {id} = req.params;
 
-//delete profile info
+    Upload.find()
+        .populate("comments likes")
+        .then(response => response.filter(item => item.owner.toString() === id))
+        .then(response => res.status(200).json(response) )
+        .catch(err=> console.log(err));
 
-//get profile by ID
+})
 
-//post on your profile
 
-//comment n like on other profiles
 
+module.exports = router;
