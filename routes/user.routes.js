c8455822c4b3eb7e14746c4425f6d64f7bbea990
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-21 18:21:01 +0200

    trying to debug user details

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -25,10 +25,11 @@ router.post("/profile", (req,res) => {
 })
 
 router.get("/:id/details", (req,res) => {
+    console.log("We can See User Details");
     const {id} = req.params
     User.findById(id)
     .then(response => res.status(200).json(response))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err));  
  })
 
 
