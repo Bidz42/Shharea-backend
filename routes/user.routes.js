1c537a6ce1ea82890ca13367004a1509faf6e03c
Bidz42 <abid97r@gmail.com>
2022-09-21 15:56:25 +0100

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-const router = express.Router();
-const User = require("../models/User.model");
-const Upload = require("../models/Upload.model");
-const Comment = require("../models/Comment.model");
-const uploadCloud = require("../config/cloudinary.config");
-const { isAuthenticated } = require("../middleware/jwt.middleware");
-
-router.get("/profile/:id", (req,res) => {
-
-})
-
-
-=======
 const Upload = require("../models/Upload.model");
 const router = express.Router();
 const User = require("../models/User.model");
@@ -23,16 +9,38 @@ const User = require("../models/User.model");
 
 router.get("/profile/:id", (req,res) => {
     const {id} = req.params;
-
     Upload.find()
         .populate("comments likes")
         .then(response => response.filter(item => item.owner.toString() === id))
         .then(response => res.status(200).json(response) )
         .catch(err=> console.log(err));
-
 })
 
+router.post("/profile", (req,res) => {
+    const {userId, location, info} = req.body
+    User.updateOne({ _id : userId }, {$set: { location: location, info: info}})
+    .then((response) => {return User.findById(userId) })
+    .then((response) => res.status(200).json(response))
+    .catch((err) => console.log(err))
+})
 
->>>>>>> 695086aae2ccc1d36b0488981f8ba1577f91a4dd
+router.get("/details", (req,res) => {
+    const {id} = req.body
+    User.findById(id)
+    .then((response) => res.status(200).json(response))
+    .catch((err) => console.log(err))
+ })
+
+
+ 
+//     const {id} = req.body
+//     console.log('hello', id)
+//     User.findById(id)
+//     .then((response) => {
+//         console.log('resp', response)
+//         res.status(200).json(response)})
+//     .catch((err) => console.log(err))
+// })
+
 
 module.exports = router;
